533bdb821cb3a44267da8ba6a21456647de16bea
KhanzaidGithub <zaidscestudent@gmail.com>
2024-11-21 07:46:19 -0800

    NodeMailer Practic

---
+++ Node.js/NodeMailer/Index.js
@@ -0,0 +1,10 @@
+var http = require('http');
+
+
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': "text/html" })
+    console.log('Hellwo world');
+    res.write('Hello world')
+    res.end();
+
+}).listen(8080)
