1b3dee47278f61f845f06d505b0258daf559c896
KhanzaidGithub <zaidscestudent@gmail.com>
2025-04-06 19:18:03 -0700

    .

--- Weather-App/src/Pages/Home.jsx
+++ Weather-App/src/Pages/Home.jsx
@@ -14,21 +14,39 @@ import Wind from './../assets/wind.png'
 
 const Home = () => {
     const [cityname, setCityName] = useState('');
-    const [data, setData] = useState(null)
-
+    const [data, setData] = useState([])
+    const [show, setShow] = useState(false)
+    const [weatherIcon, setWeatherIcon] = useState(Clear)
+    const [temp, setTemop] = useState()
     let apiKey = "88c22151468d8609cbdce07b808891ff";
 
     function submitCityName(e) {
         setCityName(' ')
         fetchData();
+        setShow(true)
     }
 
     const fetchData = async () => {
         try {
             const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${apiKey}&units=metric`)
             const data = await response.json();
+            setTemop(data.main.temp)
+
             setData(data)
-            console.log(data.weather[0].main)
+
+            if (data.weather[0].main == "Clouds") {
+                setWeatherIcon(Clouds)
+            } else if (data.weather[0].main == "Clear") {
+                setWeatherIcon(Clear)
+            } else if (data.weather[0].main == "Rain") {
+                setWeatherIcon(Rain)
+            } else if (data.weather[0].main == "Drizzle") {
+                setWeatherIcon(Drizzle)
+            } else if (data.weather[0].main == "Mist") {
+                setWeatherIcon(Mist)
+            }
+
+
         } catch (error) {
             console.log('Could not get the data')
         }
@@ -41,37 +59,46 @@ const Home = () => {
                 <div className="search">
                     <input type="text" value={cityname} placeholder='Enter City Name' onChange={(e) =>
                         setCityName(e.target.value)} />
-                    <button onClick={submitCityName ? '' : ' '} type="button">
+                    <button onClick={submitCityName} type="button">
                         <img src={Search} alt="" />
                     </button>
                 </div>
+
                 {/* <div className="error">
                     <p>Invalid City Name</p>
                 </div> */}
-                <div className={`weather `}>
-                    <img src={Clear} alt="Clear" className="weather-icon" />
-                    <h1 className="temp">22°c</h1>
-                    <h2 className="city">{cityname}</h2>
-                    <div className="details">
-                        <div className="col">
-                            <img src="images/humidity.png" alt="" className="icon" />
-                            <div>
-                                <p className="humidity">50%</p>
-                                <p>Humidity</p>
+
+                {show && data && (
+                    <div className={"weather"}>
+                        <img src={weatherIcon} alt="WeatherIcon" className="weather-icon" />
+                        <h1 className="temp">{temp}°c</h1>
+                        <h2 className="city">{data.name}</h2>
+                        <div className="details">
+                            <div className="col">
+                                <img src="images/humidity.png" alt="" className="icon" />
+                                <div>
+                                    <p className="humidity">50%</p>
+                                    <p>Humidity</p>
+                                </div>
                             </div>
-                        </div>
-                        <div className="col">
-                            <img src="images/wind.png" alt="" />
-                            <div>
-                                <p className="wind">15 km/h</p>
-                                <p>Wind Speed</p>
+                            <div className="col">
+                                <img src="images/wind.png" alt="" />
+                                <div>
+                                    <p className="wind">15 km/h</p>
+                                    <p>Wind Speed</p>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
+                )}
+
+                {/* ====== */}
+                {/* () is used for invoking functions, defining anonymous functions, grouping expressions, and wrapping JSX in React. */}
+
             </div>
         </div>
     )
 }
 
 export default Home
+
