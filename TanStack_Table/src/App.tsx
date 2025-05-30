874035b7237f7db657264988f7e5cecc27345b70
zaidkhan-div <zaidscestudent@gmail.com>
2025-05-30 14:12:58 +0500

    Started TanStack learning Buiilding simple Table then dynamic table

---
+++ TanStack_Table/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      Hi This TanStack Learning Building Simple Table then Dynamic 
+    </div>
+  )
+}
+
+export default App
