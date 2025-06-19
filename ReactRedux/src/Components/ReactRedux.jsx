1478a4b7863ab77e75795004189173b8a179964b
zaidkhan-div <zaidscestudent@gmail.com>
2025-06-19 19:30:05 +0500

    basic concept

--- ReactRedux/src/Components/ReactRedux.jsx
+++ ReactRedux/src/Components/ReactRedux.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { increament, updateValue, resetValue } from '../Features/Counter/CounterSlice.js'
 
 const ReactRedux = () => {
-    const countValue = useSelector((state) => state.counter.value)
-    console.log(countValue);
+
+    // const value = useSelector((state) => state.counter.value)
+    const dispatch = useDispatch();
+    // const checkin = dispatch(updateValue(2))
+    const value = useSelector((state) => state.counter.value)
+    useEffect(() => {
+        dispatch(updateValue(2))
+    }, [dispatch])
+    
+    const handleClick = () => {
+        dispatch(resetValue());
+        alert(value)
+    }
+    console.log(value);
 
     return (
         <React.Fragment>
-
+            <p>{value}</p>
+            <button onClick={handleClick}>ResetValue</button>
         </React.Fragment>
     )
 }
--- ReactRedux/src/Features/Counter/CounterSlice.js
+++ ReactRedux/src/Features/Counter/CounterSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from '@reduxjs/toolkit'
 const CounterSlice = createSlice({
     name: 'counter', // name used in action types
     initialState: { value: 0 },
-    reducer: { // reducer is just a object where each key is a function
+    reducers: { // reducer is just a object where each key is a function
         increament: (state) => {
             state.value = 1;
             // state	Current value of your slice
@@ -11,12 +11,25 @@ const CounterSlice = createSlice({
         },
         updateValue: (state, action) => {
             state.value = action.payload
+        },
+        resetValue: (state) => {
+            state.value = 0
         }
         // You always get state, but you only need action when you want to use data passed from the component.
     }
+
 })
 
+
+// const math = {
+//     add: (x, y) => x + y,
+//     divide: (x, y) => x / y
+// }
+// let result = math.divide(5, 5);
+// console.log(result);
+
+
 export default CounterSlice.reducer   // In the store to handle state updates
-export const { increament, updateValue } = CounterSlice.actions // In components to dispatch actions
+export const { increament, updateValue, resetValue } = CounterSlice.actions // In components to dispatch actions
 
 
